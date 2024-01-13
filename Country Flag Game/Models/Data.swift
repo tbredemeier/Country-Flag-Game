39d5fc9669ed1bed910b854ba0a949e10d58fcb5
Tom Bredemeier <tbredemeier@barrington220.org>
2024-01-13 16:32:42 -0600

    Created Data and Game Manager

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Tom Bredemeier on 1/13/24.
+//
+
+import Foundation
---
+++ Country Flag Game/Models/GameManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameManager.swift
+//  Country Flag Game
+//
+//  Created by Tom Bredemeier on 1/13/24.
+//
+
+import Foundation
