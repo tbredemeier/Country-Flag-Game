f5ef97d3d59b5f8ae3dd420ccaa9f7185fa3684e
Tom Bredemeier <tbredemeier@barrington220.org>
2024-01-13 15:50:45 -0600

    Added QuestionView

---
+++ Country Flag Game/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  Country Flag Game
+//
+//  Created by Tom Bredemeier on 1/13/24.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
