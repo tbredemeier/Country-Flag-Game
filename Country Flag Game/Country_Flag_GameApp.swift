8ae08edf2736b0c23c0c2e13f6e99df5bdd12bf1
Tom Bredemeier <tbredemeier@barrington220.org>
2026-01-08 11:12:20 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Tom Bredemeier on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
